5c62bbf6580f1f3c01fba9ba817c5af3ae3b5ebe
hcwang24 <hcwang24@student.ubc.ca>
2024-09-10 04:02:11 -0700

    Beautifying the dashboard and updating readme

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# BC_Forests
+# Forest and Wildfire Dashboard
+
+## Project Overview
+
+The Forest and Wildfire Dashboard is an interactive web application built with Python Dash and Plotly to visualize and analyze wildfire incidents and forest coverage in British Columbia (BC), Canada. This dashboard allows users to explore wildfire data, view forest coverage, and analyze fire status and causes across different forest regions.
+
+### Features
+- Interactive map showing wildfire locations and forest boundaries.
+- Date range slider to filter wildfires by ignition and termination dates.
+- Detailed bar plots showing the distribution of fire statuses and causes.
+- Dynamic updates based on user interactions with the map and date slider.
+
+## Installation
+
+### Prerequisites
+
+Ensure you have Python 3.7 or later installed. You will also need to install the following Python packages:
+- Dash
+- Plotly
+- Geopandas
+- Pandas
+- NumPy
+- Dash Bootstrap Components
+
+You can install the required packages using pip:
+
+```bash
+pip install dash plotly geopandas pandas numpy dash-bootstrap-components
+```
+
+### Data Files
+
+Download the necessary data files and place them in a `data` directory within your project. The files required are:
+- `FADM_PROV_FOREST_simplified.geojson`: Forest data.
+- `PROT_CURRENT_FIRE_PNTS_SP_simplified.geojson`: Wildfire data.
+
+You can download these files from [BC Data Catalogue](https://catalogue.data.gov.bc.ca/dataset).
+
+## Usage
+
+To run the dashboard, execute the following Python script:
+
+```python
+python app.py
+```
+
+This will start a local server, and you can view the dashboard in your web browser at `http://127.0.0.1:8050`.
+
+### How It Works
+
+1. **Main Map**: Displays the forest and wildfire data on an interactive map. Wildfire locations are marked with different colors based on their status.
+2. **Focused Map**: Allows users to select a date range and interact with the map to view detailed information about a specific forest. The focused map updates with selected forest data, fire status, and cause.
+3. **Date Slider**: Filter the wildfire data based on the selected date range to analyze incidents within that period.
+4. **Bar Plots**: Show the distribution of fire statuses and causes based on the selected date range and forest region.
+
+## Data Sources
+
+- **Forest Data**: [BC Forest Data](https://catalogue.data.gov.bc.ca/dataset/fadm-prov-forest-simplified)
+- **Wildfire Data**: [BC Wildfire Data](https://catalogue.data.gov.bc.ca/dataset/prot-current-fire-pnts-sp-simplified)
+
+## Acknowledgments
+
+This project was created by HanChen Wang in 2024. The data used is publicly available and provided by the Government of British Columbia.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
--- dashboard.py
+++ dashboard.py
@@ -5,19 +5,24 @@ from dash.dependencies import Input, Output
 import geopandas as gpd
 import numpy as np
 import warnings
+import pandas as pd
+from datetime import datetime
+import dash_bootstrap_components as dbc
 
 # Suppress specific warnings
 warnings.simplefilter(action='ignore', category=UserWarning)
 
 # Initialize Dash app
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.JOURNAL])
 
 # Load the data
 forest_data = gpd.read_file("data/FADM_PROV_FOREST_simplified.geojson")
 wildfire_data = gpd.read_file("data/PROT_CURRENT_FIRE_PNTS_SP_simplified.geojson")
 
 # Fill missing Termination_Date with "still active" if any are missing
-wildfire_data['Termination_Date'].fillna("still active", inplace=True)
+wildfire_data['Termination_Date_Label']=wildfire_data['Termination_Date']
+wildfire_data['Termination_Date_Label'].fillna("still active", inplace=True)
+wildfire_data['Termination_Date'].fillna(pd.Timestamp('now'), inplace=True)
 
 # Map FIRESTATUS to colors
 status_color_map = {
@@ -57,8 +62,8 @@ def create_forest_map():
     ))
     fig.update_layout(
         mapbox_style="open-street-map",
-        mapbox_zoom=4.3,
-        mapbox_center={"lat": 53.7267, "lon": -125.6476},
+        mapbox_zoom=4.65,
+        mapbox_center={"lat": 54.7267, "lon": -125.6476},
         margin={"r": 0, "t": 0, "l": 0, "b": 0},
         title="Forest and Wildfire Data Visualization in BC",
         height=800
@@ -81,17 +86,37 @@ def create_bar_plot(data, category, x_title, selected_forest=None):
     )
     return bar_fig
 
+# Add date range for the slider
+min_date = wildfire_data['Ignition_Date'].min().date()
+max_date = pd.Timestamp('now').date()
+
 app.layout = html.Div([
-    html.Div([
-        html.H1("Forest and Wildfire Dashboard"),
-        html.P("Explore wildfire incidents and forest coverage in British Columbia. Use this dashboard to understand the distribution and characteristics of wildfires across different forest regions."),
-    ], style={'width': '100%', 'display': 'inline-block', 'verticalAlign': 'top', 'textAlign': 'center', 'padding': '10px'}),
+    html.Header([
+        html.Div([
+            html.H1("Forest and Wildfire Dashboard"),
+            html.P("Explore wildfire incidents and forest coverage in British Columbia. Use this dashboard to understand the distribution and characteristics of wildfires across different forest regions."),
+        ])], style={'width': '100%', 'display': 'inline-block', 'verticalAlign': 'top', 'textAlign': 'center', 'padding': '10px', 'background-color': '#f1f1f1'}),
     
     html.Div([
-        dcc.Graph(id='forest-map', figure=create_forest_map(), style={'width': '100%', 'height': '100%'}),
+        html.Div([
+            html.H2("Forest Map"),
+            dcc.Graph(id='forest-map', figure=create_forest_map(), style={'width': '100%', 'height': '100%'}),
+        ], style={'border': '1px solid #ddd', 'border-radius': '8px', 'padding': '15px', 'margin-bottom': '15px', 'box-shadow': '0 4px 8px rgba(0,0,0,0.1)', 'background-color': '#fff'}, className='card'),
     ], style={'width': '50%', 'height': '100%', 'display': 'inline-block', 'verticalAlign': 'top', 'padding': '10px'}),
 
     html.Div([
+        html.Div([
+            html.H2("Focused Map"),
+        dcc.RangeSlider(
+            id='date-slider',
+            min=int(datetime.strptime(str(min_date), '%Y-%m-%d').timestamp()),
+            max=int(datetime.strptime(str(max_date), '%Y-%m-%d').timestamp()),
+            step=100,  # Step of one day
+            marks={int(datetime.strptime(str(min_date), '%Y-%m-%d').timestamp()): min_date.strftime('%Y-%m-%d'),
+                   int(datetime.strptime(str(max_date), '%Y-%m-%d').timestamp()): max_date.strftime('%Y-%m-%d')},
+            value=[int(datetime.strptime(str(min_date), '%Y-%m-%d').timestamp()), int(datetime.strptime(str(max_date), '%Y-%m-%d').timestamp())]
+        ),
+        html.Div(id='selected-date-range', style={'padding': '10px', 'font-size': '16px'}),
         html.Div([
             dcc.Graph(id='selected-forest-map', style={'width': '100%', 'height': '50%'}),
         ], style={'width': '100%', 'height': '40%'}),
@@ -100,34 +125,53 @@ app.layout = html.Div([
             dcc.Graph(id='fire-status-bar', style={'width': '50%', 'display': 'inline-block', 'height': '60%'}),
             dcc.Graph(id='fire-cause-bar', style={'width': '50%', 'display': 'inline-block', 'height': '60%'}),
         ], style={'width': '100%', 'display': 'flex', 'height': '60%'}),
+        ], style={'border': '1px solid #ddd', 'border-radius': '8px', 'padding': '15px', 'margin-bottom': '15px', 'box-shadow': '0 4px 8px rgba(0,0,0,0.1)', 'background-color': '#fff'}, className='card'),
     ], style={'width': '40%', 'height': '100%', 'display': 'inline-block', 'verticalAlign': 'top', 'padding': '10px'}),
 
     html.Footer([
-        html.Div("Created by HanChen Wang, 2024", style={'text-align': 'center', 'padding': '10px', 'font-size': '14px', 'color': '#888'}),
+        html.Div("Created by HanChen Wang, 2024. Data is publicly available for download at https://catalogue.data.gov.bc.ca/dataset", 
+                 style={'text-align': 'center', 'padding': '10px', 'font-size': '14px', 'color': '#888'}),
     ], style={'width': '100%', 'background-color': '#f1f1f1', 'padding': '10px', 'position': 'fixed', 'bottom': 0}),
-])
+], style={
+    'background-image': 'url("https://www2.gov.bc.ca/assets/gov/farming-natural-resources-and-industry/forestry/forest-future/forest001-01.png")',
+    'background-size': 'cover',
+    'background-repeat': 'no-repeat',
+    'background-attachment': 'fixed',
+    'background-position': 'center',
+    'min-height': '100vh',
+})
 
 @app.callback(
-    [Output('fire-status-bar', 'figure'),
+    [Output('selected-date-range', 'children'),
+     Output('fire-status-bar', 'figure'),
      Output('fire-cause-bar', 'figure'),
      Output('selected-forest-map', 'figure')],
     [Input('forest-map', 'clickData'),
-     Input('forest-map', 'hoverData')]
+     Input('forest-map', 'hoverData'),
+     Input('date-slider', 'value')]
 )
-def update_plots(clickData, hoverData):
+def update_plots(clickData, hoverData, date_range):
+    start_date = datetime.fromtimestamp(date_range[0])
+    end_date = datetime.fromtimestamp(date_range[1])
+
+    filtered_wildfire_data = wildfire_data[
+        (wildfire_data['Ignition_Date'] <= end_date) & 
+        (wildfire_data['Termination_Date'] >= start_date)
+    ]
+
     selected_forest = None
     if clickData:
         selected_forest = clickData['points'][0]['hovertext'].split("<br>")[0].split(": ")[1]
     elif hoverData:
         selected_forest = hoverData['points'][0]['hovertext'].split("<br>")[0].split(": ")[1]
     
-    fire_status_bar = create_bar_plot(wildfire_data, 'FIRESTATUS', "Fire Status", selected_forest)
-    fire_cause_bar = create_bar_plot(wildfire_data, 'FIRE_CAUSE', "Cause of Fire", selected_forest)
+    fire_status_bar = create_bar_plot(filtered_wildfire_data, 'FIRESTATUS', "Fire Status", selected_forest)
+    fire_cause_bar = create_bar_plot(filtered_wildfire_data, 'FIRE_CAUSE', "Cause of Fire", selected_forest)
     
     forest_zoomed_map = go.Figure()
     if selected_forest:
         selected_forest_data = forest_data[forest_data['Forest_Name'] == selected_forest]
-        selected_wildfire_data = wildfire_data[wildfire_data['Forest_Name'] == selected_forest]
+        selected_wildfire_data = filtered_wildfire_data[filtered_wildfire_data['Forest_Name'] == selected_forest]
         
         if not selected_wildfire_data.empty:
             size_ha_log = np.log1p(selected_wildfire_data['SIZE_HA'])
@@ -158,7 +202,7 @@ def update_plots(clickData, hoverData):
                             f"Size: {row['SIZE_HA']} hectares<br>"
                             f"Cause: {row['FIRE_CAUSE']}<br>"
                             f"Ignition: {row['Ignition_Date']}<br>"
-                            f"Termination: {row['Termination_Date']}", axis=1),
+                            f"Termination: {row['Termination_Date_Label']}", axis=1),
             hoverinfo='text',
             name='Wildfire Locations'
         ))
@@ -169,10 +213,10 @@ def update_plots(clickData, hoverData):
             mapbox_center={"lat": selected_forest_data.geometry.centroid.y.values[0],
                            "lon": selected_forest_data.geometry.centroid.x.values[0]},
             margin={"r": 0, "t": 0, "l": 0, "b": 0},
-            height=600
+            height=450
         )
 
-    return fire_status_bar, fire_cause_bar, forest_zoomed_map
+    return f"Selected Date Range: {start_date.strftime('%Y-%m-%d')} to {end_date.strftime('%Y-%m-%d')}", fire_status_bar, fire_cause_bar, forest_zoomed_map
 
 if __name__ == '__main__':
     app.run_server(debug=True)
